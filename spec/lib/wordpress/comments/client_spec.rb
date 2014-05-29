f108e48ddd22d0b7c31268db239b2a3fc851fb5a
Kyle Geske <stungeye@gmail.com>
2014-05-29 15:24:05 -0400

    First working rspec example.

--- spec/lib/wordpress/comments/client_spec.rb
+++ spec/lib/wordpress/comments/client_spec.rb
@@ -1,11 +1,19 @@
 module Wordpress
   module Comments
     class Client
+      attr_reader :url
+
+      def initialize url
+        @url = url
+      end
     end
   end
 end
 
 
 describe Wordpress::Comments::Client do
-  
+  it "stores a URL" do
+    client = Wordpress::Comments::Client.new 'http://mashable.com/comments/feed'
+    expect(client.url).to eq 'http://mashable.com/comments/feed'
+  end
 end
