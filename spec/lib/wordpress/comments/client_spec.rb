55c7bc62dfff31b1196ca4d1fb716ef56d4e8588
Kyle Geske <stungeye@gmail.com>
2014-05-29 15:33:55 -0400

    Added a xml fixture for a feed.

--- spec/lib/wordpress/comments/client_spec.rb
+++ spec/lib/wordpress/comments/client_spec.rb
@@ -12,8 +12,10 @@ end
 
 
 describe Wordpress::Comments::Client do
-  it "stores a URL" do
-    client = Wordpress::Comments::Client.new 'http://mashable.com/comments/feed'
-    expect(client.url).to eq 'http://mashable.com/comments/feed'
+  describe "#initialize" do
+    it "stores a URL" do
+      client = Wordpress::Comments::Client.new 'http://mashable.com/comments/feed'
+      expect(client.url).to eq 'http://mashable.com/comments/feed'
+    end
   end
 end
